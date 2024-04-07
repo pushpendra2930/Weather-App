4ae536df0c3acd3a262e0986c6b717aacd971b8e
Pushpendra Singh Rathore <144723436+pushpendra2930@users.noreply.github.com>
2024-04-07 23:17:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+
+async function checkWeather(city){
+    const api_key = "523aeb2fc388232667c142c522aa78e9";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response=> response.json());
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`
+
+    switch (weather_data.weather[0].main) {
+        case 'Clouds':
+            weather_img.src = "cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "mist.png";
+            break;
+    }
+
+
+}
+
+searchBtn.addEventListener('click',()=>{
+    checkWeather(inputBox.value);
+});
+
